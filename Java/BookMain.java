8e37100c8c638dd83bdb855b5329eef9d7d5e901
Elzen Rein Marco Maceda <152077870+azaleamiku@users.noreply.github.com>
2025-08-15 15:03:05 +0800

    Delete Java directory

--- Java/BookMain.java
+++
@@ -1,9 +0,0 @@
-public class BookMain {
-    public static void main(String[] args) {
-        BookClass b1 = new BookClass("Pluma", "Elzen");
-        BookClass b2 = new BookClass("Science", "Einstein");
-
-        b1.bookShelf();
-        b2.bookShelf();
-    }
-}
--- Java/Brk.java
+++
@@ -1,9 +0,0 @@
-public class Brk {
-    public static void main(String[] args) {
-
-        for(int i = 0; i < 5; i++) {
-            if (i == 3) break;
-            System.out.println(i);
-        }
-    }
-}
